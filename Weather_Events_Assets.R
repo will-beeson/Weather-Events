b9f43017b8186b0e9b32f2c871174eb0a65c258e
Will Richards <will@beesonconsulting.co.uk>
2022-10-26 09:42:34 +0100

    Initial comments on R script, testing git connection

---
+++ Weather_Events_Assets.R
@@ -0,0 +1,5 @@
+# R Script to extract weather events from tableau_asset, retain "significn"significant" events by day and asset
+# and aggregate to a policy level
+# NO loss assumptions at this point
+# Intended only to display event and loss assumption can be made based on maximum limit (by locations exposed)
+
